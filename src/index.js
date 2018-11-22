7f4a656284f2c9c7b03c0bdd29355de293b1911a
yulei <yulei@addcn.com>
2018-11-22 10:12:01 +0800

    完善贷款试算注释

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 /**
+ * 重要概念和字段:
+ * 1. 利率类别分为两种:
+ *  a.单一利率 字段 无, 使用 rate 内的值
+ *  b.多段利率 字段 Multi, 使用 segment 内的值
+ * 2. 还款类别分为两种:
+ *  a.本息均摊 字段 interest type: 1
+ *  b.本金均摊 字段 capital type: 2
+ * 3. 数据返回
+ *  a. base 方法返回单一利率的计算结果
+ *  b. 想要获取单一利率每月还款明细, 需要使用 interestPerMonthList 或 capitalPerMonthList 方法
+ *  c. 多段利率使用 interestMulti 或 capitalMulti 方法, 会同时返回基础计算结果和每月还款明细
+ *
  * @param price 貸款金額
  * @param month 貸款期限
  * @param type 貸款類型 {'本息定額': 1, '本金定額': 2}
@@ -27,6 +39,7 @@ class MortgageCalculator {
         this.params = Object.assign({}, settings, params)
     }
 
+    // 基本方法, 单一利率获取计算结果, type = 2 可获取本金均摊计算结果
     base (params) {
         let { price, month, type, graceMonth, rate } = Object.assign({}, this.params, params)
 
@@ -97,6 +110,7 @@ class MortgageCalculator {
         }
     }
 
+    // 本息均摊 - 单一利率下获取每月明细
     interestPerMonthList (params) {
         var result = this.base(Object.assign({}, this.params, params))
 
@@ -126,6 +140,7 @@ class MortgageCalculator {
         return monthList
     }
 
+    // 本金均摊 - 单一利率下获取每月明细
     capitalPerMonthList (params) {
         var result = this.base(Object.assign({}, this.params, params, {type: 2}))
 
